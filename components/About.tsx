fc90e7acb065129b32d3f5b9814b9086e4a3c54e
armanyfelix <armanyfelix@outlook.com>
2021-11-26 19:31:54 -0800

    another resumen

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ function About() {
         <section id="about" className="w-full lg:px-52 min-h-screen lg:p-14 md:p-10 p-8 bg-gray-900 ">
             <div className="justify-between items-center xl:mx-20 text-center flex sm:m-2 md:m-3 lg:m-4 xl:m-5 ">
                 <h1 className="text-white lg:text-5xl md:text-5xl sm:text-3xl text-3xl items-center font-simplex font-bold">About Me</h1>
-                <Link href="https://docs.google.com/document/d/1IR4Lu-3_kMMN260gGagu_1yFdUmiHrfAEqbbxYKsUqc/edit?usp=sharing">
+                <Link href="https://docs.google.com/document/d/1VhnkfTEZcFpjd51uswrxccZlWiCBumZw/edit?usp=sharing&ouid=106059346458081499165&rtpof=true&sd=true">
                     <button className={resumen.btn}>
                         <span className="font-mono">Resumen</span>
                     </button>
@@ -28,7 +28,7 @@ function About() {
                     </p>
                     <br />
                     <p className="text-justify lg:text-lg md:text-base text-sm text-white">
-                        I have more of  2 years  working as a freelancer, I make websites and other freelancers, I offer them scalable and flexible software, so they can improve their service while their business grows.
+                        I have been working as a freelancer for more than 2 years, I make websites for businesses and other freelancers. I try to find the best and most profitable solution for clients with scalable, easy to maintain, responsive and modern software.
                     </p>
                     <br />
                     <p className="text-justify lg:text-lg md:text-base text-sm text-white">
